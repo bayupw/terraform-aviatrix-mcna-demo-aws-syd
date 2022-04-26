e890978e072241947171fb1a4f3018a964ffe51e
bayupw <bayupw@gmail.com>
2022-04-27 09:01:15 +1200

    update output

--- outputs.tf
+++ outputs.tf
@@ -10,14 +10,14 @@ output "aws_syd_shared01_ssm_session" {
   sensitive   = false
 }
 
-output "aws_syd_shared01_instance_id" {
-  description = "AWS Sydney Dev01 Instance"
-  value       = module.aws_sgp_dev01_instance.aws_instance.id
+output "aws_syd_prod01_instance_id" {
+  description = "AWS Sydney Prod01 Instance"
+  value       = module.aws_syd_prod01_instance.aws_instance.id
   sensitive   = false
 }
 
-output "aws_syd_shared01_ssm_session" {
-  description = "AWS Sydney Dev01 Instance"
-  value       = "aws ssm start-session --region ${data.aws_region.current.name} --target ${module.aws_sgp_dev01_instance.aws_instance.id}"
+output "aws_syd_prod01_ssm_session" {
+  description = "AWS Sydney Prod01 Instance"
+  value       = "aws ssm start-session --region ${data.aws_region.current.name} --target ${module.aws_syd_prod01_instance.aws_instance.id}"
   sensitive   = false
 }
